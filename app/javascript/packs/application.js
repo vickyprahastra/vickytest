ea80763b54330b10342f37c4c6161055e12e3b9b
Vicky Prahastra <vickyprahastra@gmail.com>
2021-07-17 14:20:04 +0700

    Bootstrap theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,5 +16,29 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
+import {WOW} from 'wow.js';
 import "css/application";
+import "../js/owl-carousel.js";
+import "../js/animation.js";
+import "../js/imagesLoaded.js";
+import "../js/custom.js";
+// import "../js/isotope.js";
+// import "../js/tabs.js";
+
+
+
+// export default {
+//
+//   init() {
+//     // JavaScript to be fired on all pages
+//     const wow = new Wow();
+//     wow.init();
+//   },
+//
+//   finalize() {
+//
+//   // JavaScript to be fired on all pages, after page specific JS is fired
+//
+//   },
+//
+// };
