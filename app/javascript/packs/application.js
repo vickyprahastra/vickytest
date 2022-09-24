1e2f402e2167fa4802d2f6484f13c2b368db215f
Vicky Prahastra <vickyprahastra@gmail.com>
2022-09-24 19:45:44 +0700

    Make simple UI integration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,37 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require.context('../images', true)
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-import {WOW} from 'wow.js';
 import "css/application";
-import "../js/owl-carousel.js";
-import "../js/animation.js";
-import "../js/imagesLoaded.js";
-import "../js/custom.js";
-// import "../js/isotope.js";
-// import "../js/tabs.js";
 
 
-
-// export default {
-//
-//   init() {
-//     // JavaScript to be fired on all pages
-//     const wow = new Wow();
-//     wow.init();
-//   },
-//
-//   finalize() {
-//
-//   // JavaScript to be fired on all pages, after page specific JS is fired
-//
-//   },
-//
-// };
