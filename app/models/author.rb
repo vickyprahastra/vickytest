973f1bb95b5fa78d64b1c6918e6e40447e132eda
Vicky Prahastra <vickyprahastra@gmail.com>
2022-10-19 12:32:03 +0700

    Fix edit array datas

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,18 @@
 class Author < ApplicationRecord
   enum gender: { n: 0, m: 1, f: 2 }
-  # jsonb_accessor :wikipedia,
-  #   url: :string,
-  #   found: :boolean
 
+  after_save :parse_json
+  after_create :create_id
+
+  def parse_json
+    return if books.is_a? Array
+    return unless books
+
+    books = JSON.parse(self.books)
+    update_columns(books: books)
+  end
+
+  def create_id
+    update_columns(int_id: Author.count + 1)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,16 @@
 class Book < ApplicationRecord
-  jsonb_accessor :wikipedia,
-    url: :string,
-    found: :boolean
+  after_save :parse_json
+  after_create :create_id
+
+  def create_id
+    update_columns(int_id: Author.count + 1)
+  end
+
+  def parse_json
+    return if authors.is_a? Array
+    return unless authors
+
+    authors = JSON.parse(self.authors)
+    update_columns(authors: authors)
+  end
 end
