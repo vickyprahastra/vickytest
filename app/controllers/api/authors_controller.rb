0a9006f71a67601c6f0ce38cb6facaf6833c2ed8
Vicky Prahastra <vickyprahastra@gmail.com>
2022-10-24 12:41:45 +0700

    Fix search the authors name

--- app/controllers/api/authors_controller.rb
+++ app/controllers/api/authors_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class AuthorsController < ActionController::API
     def search
-      authors = Author.where('name LIKE ?', "%#{params[:name]}%").select('name, int_id').take(5)
+      authors = Author.where('LOWER(name) LIKE ?', "%#{params[:name]}%").select('name, int_id').take(5)
 
       json_response_success(authors.to_json)
     end
